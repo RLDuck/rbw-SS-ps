07e248697e6f777dad49d181d6d92686e25c6bde
OG <ogcontentpaypal@gmail.com>
2025-12-09 15:02:42 +0300

    Update Services.ps1

--- Services.ps1
+++ Services.ps1
@@ -76,7 +76,8 @@ $settings = @(
     @{ Name = "PowerShell Logging"; Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"; Key = "EnableScriptBlockLogging"; Warning = "Disabled"; Safe = "Enabled" },
     @{ Name = "Activities Cache"; Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System"; Key = "EnableActivityFeed"; Warning = "Disabled"; Safe = "Enabled" },
     @{ Name = "Prefetch Enabled"; Path = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters"; Key = "EnablePrefetcher"; Warning = "Disabled"; Safe = "Enabled" }
-    @{ Name="Pca Key"; Paths=@("HKCU:\SOFTWARE\Policies\Microsoft\Windows\AppCompat","HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat"); Key="DisablePCA"; Warning="Disabled"; Safe="Enabled" }
+    @{ Name = "Pca Key (HKCU)"; Path = "HKCU:\SOFTWARE\Policies\Microsoft\Windows\AppCompat"; Key = "DisablePCA"; Warning = "Disabled"; Safe = "Enabled" },
+    @{ Name = "Pca Key (HKLM)"; Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat"; Key = "DisablePCA"; Warning = "Disabled"; Safe = "Enabled" }
 )
 
 foreach ($s in $settings) {
